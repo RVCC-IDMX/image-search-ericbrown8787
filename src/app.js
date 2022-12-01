d0de0bf0925237ee59b5f5b3a4a54315b75bd7bc
Eric Brown <eric.brown8787@gmail.com>
2022-12-01 13:20:30 -0500

    Change innerText to textContent
    Probably a better tool for the job

--- src/app.js
+++ src/app.js
@@ -19,45 +19,48 @@ form.addEventListener('submit', async (event) => {
     .then((res) => res.json())
     .catch((err) => console.error(err));
 
-  response.results.forEach((item) => {
-    const cardClone = document.querySelector('#template').content.firstElementChild.cloneNode(true);
-    const postImg = cardClone.querySelector('.post__img');
-    const postUser = cardClone.querySelector('.post__user');
-    const postDescription = cardClone.querySelector('.post__desc');
-    let formattedDescription = item.description;
-    if (formattedDescription) {
-      if (typeof formattedDescription === 'string' && formattedDescription.length > 100) {
-        formattedDescription = `${formattedDescription.slice(0, 100)}...`;
+  /*
+  Loop through the results[] array. For each result, create a clone of the
+  template and append it to the DOM element with the .container class.
+  */
+  if (response.results) {
+    response.results.forEach((item) => {
+      const cardClone = document.querySelector('#template').content.firstElementChild.cloneNode(true);
+      const postImg = cardClone.querySelector('.post__img');
+      const postUser = cardClone.querySelector('.post__user');
+      const postDescription = cardClone.querySelector('.post__desc');
+
+      /*
+      Add an attribution statement below the image using the
+      postUser element and the photographer's name from dataObj
+      */
+      if (item.user.first_name || item.user.last_name) {
+        postUser.textContent = `by ${item.user.first_name || ''} ${item.user.last_name || ''}`;
       }
-    } else {
-      formattedDescription = '';
-    }
 
-    postImg.src = item.urls.small;
-    postImg.alt = item.alt_description;
+      /*
+      Check the description of the post. If it's bot bull and less than 100 characters,
+      add the description from dataObj to the post. If it's more than 100 characters,
+      add the first 100 characters of the description from dataObj to the post followed by
+      an ellipsis (...)
+      */
+      let formattedDescription = item.description;
+      if (formattedDescription) {
+        if (typeof formattedDescription === 'string' && formattedDescription.length > 100) {
+          formattedDescription = `${formattedDescription.slice(0, 100)}...`;
+        }
+      } else {
+        formattedDescription = '';
+      }
 
-    if (item.user.first_name || item.user.last_name) {
-      postUser.innerText = `by ${item.user.first_name || ''} ${item.user.last_name || ''}`;
-    }
+      postImg.src = item.urls.small;
+      postImg.alt = item.alt_description;
 
-    postDescription.innerText = `${formattedDescription}`;
+      postDescription.textContent = `${formattedDescription}`;
 
-    container.appendChild(cardClone);
-  });
-  /*
-    Loop through the results[] array. For each result, create a clone of the
-    template and append it to the DOM element with the .container class.
-  */
-
-  /*
-    Add an attribution statement below the image using the
-    postUser element and the photographer's name from dataObj
-   */
-
-  /*
-    Check the description of the post. If it's bot bull and less than 100 characters,
-    add the description from dataObj to the post. If it's more than 100 characters,
-    add the first 100 characters of the description from dataObj to the post followed by
-    an ellipsis (...)
-  */
+      container.appendChild(cardClone);
+    });
+  } else {
+    container.textContent = 'Oops! We were unable to retrieve your results. :(';
+  }
 });
